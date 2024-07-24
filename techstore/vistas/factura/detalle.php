840e8910813e9d9251fe7547dfeb12f5ef070c1a
Cesar David Dominguez Dominguez <davd.dmngz@gmail.com>
2024-07-23 18:57:22 -0600

    Revert "factura"
    This reverts commit c2c2b4ee33beae63015bdbdd740e8f8c2f804df3.

--- techstore/vistas/factura/detalle.php
+++
@@ -1,5 +0,0 @@
-<h1>Detalle de Factura</h1>
-<p>ID: <?php echo $factura->id; ?></p>
-<p>Fecha: <?php echo $factura->fecha; ?></p>
-<p>Total: <?php echo $factura->total; ?></p>
-<p>Método de Pago: <?php echo $factura->metodo_pago; ?></p>
--- techstore/vistas/factura/index.php
+++
@@ -1,3 +0,0 @@
-<h1>Inicio de Facturas</h1>
-<a href="?c=factura&a=crear">Crear Nueva Factura</a>
-<a href="?c=factura&a=mostrar">Ver Facturas</a>
