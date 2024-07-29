2c2b5925c9fbc969dd9db7c01b7151b92d2760e6
anettemendez <anettem347@gmail.com>
2024-07-28 22:07:52 -0600

    branchfinal
    implementar carrito

--- techstore/assets/js/load-files.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var inputFile = document.getElementById('inputGroupFile01');
-    var fileLabel = document.querySelector('.custom-file-label');
-    
-    inputFile.addEventListener('change', function(event) {
-        var fileName = event.target.files[0] ? event.target.files[0].name : 'Choose file';
-        fileLabel.textContent = fileName;
-    });
-});
--- techstore/vistas/foother.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <div class="footer-area">
-        <p>© Copyright 2024. All right reserved. Template by <a href="https://colorlib.com/wp/">Colorlib</a>.</p>
-    </div>
-</footer>
